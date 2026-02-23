6a87df7e1c02123d68d5c452d1b50a1a421142ca
albabmoran <albaben.moran@gmail.com>
2026-02-23 18:12:45 +0100

    add choosing algorithms, adaptive alg and lst ft

---
+++ list_functions.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albben-a <albben-a@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 17:11:34 by albben-a          #+#    #+#             */
+/*   Updated: 2026/02/23 18:11:38 by albben-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*ft_lstnew(int content, int index)
+{
+	t_stack	*node;
+
+	node = malloc(sizeof(t_stack));
+	if (!node)
+		return (NULL);
+	node->number = content;
+    node->index = index;
+	node->next = NULL;
+	return (node);
+}
+
+static t_stack	*ft_lstlast(t_stack *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
+
+static void	ft_lstadd_front(t_stack	**lst, t_stack *new)
+{
+	new->next = (*lst);
+	*lst = new;
+}
+
+void	ft_lstadd_back(t_stack **lst, t_stack *new)
+{
+	t_stack	*last;
+
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+		ft_lstadd_front(lst, new);
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
---
+++ push_swap.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albben-a <albben-a@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 13:28:02 by albben-a          #+#    #+#             */
+/*   Updated: 2026/02/23 18:11:40 by albben-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_stack {
+    int 			number;
+    int 			index;
+	struct s_stack	*next;
+
+} t_stack;
+
+typedef struct s_data {
+    int *stack_a;
+    int size;
+    int strategy;
+    int bench_mode;
+    double disorder;
+} t_data;
+
+//escoge el algoritmo en función de la flag
+void	choose_algorithm(t_data *data);
+//generan la estructura con la que trabajamos luego
+t_stack	**get_stack_a(t_data *data);
+void	ft_lstadd_back(t_stack **lst, t_stack *new);
+t_stack	*ft_lstnew(int content, int index);
+//función algoritmo adaptativo
+void	adaptive(t_data *data);
+
+#endif
