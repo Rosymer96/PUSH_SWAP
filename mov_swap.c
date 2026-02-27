584a0c423c73e8a0de1111c2b5117cbfae39e6b1
albabmoran <albaben.moran@gmail.com>
2026-02-27 12:48:56 +0100

    add total mov count in every movement ft

--- mov_swap.c
+++ mov_swap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   mov_swap.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albben-a <albben-a@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 10:45:55 by albben-a          #+#    #+#             */
-/*   Updated: 2026/02/25 12:15:01 by albben-a         ###   ########.fr       */
+/*   Updated: 2026/02/27 12:48:13 by albben-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	sa(t_stack *a, t_data *data)
 	swap(a);
 	write(1, "sa\n", 3);
 	data->stats.sa++;
+	data->stats.total++;
 }
 
 void	sb(t_stack *b, t_data *data)
@@ -38,6 +39,7 @@ void	sb(t_stack *b, t_data *data)
 	swap(b);
 	write(1, "sb\n", 3);
 	data->stats.sb++;
+	data->stats.total++;
 }
 
 void	ss(t_stack *a, t_stack *b, t_data *data)
@@ -46,4 +48,5 @@ void	ss(t_stack *a, t_stack *b, t_data *data)
 	swap(b);
 	write(1, "ss\n", 3);
 	data->stats.ss++;
+	data->stats.total++;
 }
