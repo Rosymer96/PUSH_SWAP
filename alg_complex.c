e969a58d3697c9bb77e58a500ba1ac9d5c7dbc51
Rosa Vela <rosymer96@gmail.com>
2026-03-02 16:16:22 +0100

    Primer avance de complex, reutilizando LIS

--- alg_complex.c
+++ alg_complex.c
@@ -12,10 +12,64 @@
 
 #include "push_swap.h"
 
+static void mark_index_lis(t_stack *a, int *lis_array, int lis_size)
+{
+	t_stack *tmp;
+	int	i;
+
+	tmp = a;
+	while(tmp)
+	{
+		tmp->index = 0;
+		i = 0;
+		while (i < lis_size)
+		{
+			if (tmp->number == lis_array[i])
+			{
+				tmp->index = -1; //marcamos los LIS
+				break;
+			}
+			i++;
+		}
+		tmp = tmp->next;
+	}
+}
+//movemos los que no son LIS a B
+static void push_non_lis(t_stack **a, t_stack **b, t_data *data)
+{
+	int i;
+    int size;
+
+    i = 0;
+	size = data->size;
+	while (i < size)
+	{
+		if ((*a)->index != -1)
+			pb(a, b, data);
+		else
+			ra(a, data);
+		i++;
+	}
+}
+
 void	complex(t_stack **a, t_stack **b, t_data *data)
 {
-	(void)a;
-	(void)b;
-	printf("%s\n", (char *)data->stack_a);
-	printf("complex");
+	int *lis_array;
+	int lis_size;
+
+	lis_array = get_lis_ind(data, &lis_size);
+
+	if (!lis_array) 
+		return ;
+	mark_index_lis(*a, lis_array, lis_size);
+	//el array ya no es necesario
+	free(lis_array);
+	//movemos los que no son de la LIS a b
+	push_non_lis(a, b, data);
+	//calcular greedy para regresar a A;
+
+
+	//Rotamos en caso sea necesario
+	final_rot(a, data);
 }
+
