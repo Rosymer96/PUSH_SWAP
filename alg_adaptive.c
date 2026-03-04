07095b33f9b96161d5d5988cf4729c9141d25e5f
albabmoran <albaben.moran@gmail.com>
2026-03-04 17:47:22 +0100

    updated adaptive alg to lst size and bench (adaptive)

--- alg_adaptive.c
+++ alg_adaptive.c
@@ -6,12 +6,12 @@
 /*   By: albben-a <albben-a@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 15:46:26 by albben-a          #+#    #+#             */
-/*   Updated: 2026/03/03 23:32:31 by albben-a         ###   ########.fr       */
+/*   Updated: 2026/03/04 17:46:39 by albben-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+/*
 void	adaptive(t_stack **a, t_stack **b, t_data *data) //algoritmo adaptativo, escoge cuál se usa en función del índice de desorden
 {
 	if (data->disorder < 0.2)
@@ -20,4 +20,30 @@ void	adaptive(t_stack **a, t_stack **b, t_data *data) //algoritmo adaptativo, es
 		medium(a, b, data);
 	else if (data->disorder >= 0.5)
 		complex(a, b, data);
+}*/
+
+void adaptive(t_stack **a, t_stack **b, t_data *data)
+{
+	if (data->size <= 5)
+	{
+		simple(a, b, data);
+		return;
+	}
+	if (data->size <= 50)
+	{
+		if (data->disorder < 0.15)
+			simple(a, b, data);
+		else
+			medium(a, b, data);
+		return;
+	}
+	if (data->size <= 200)
+	{
+		if (data->disorder < 0.1)
+			simple(a, b, data);
+		else
+			medium(a, b, data);
+		return;
+	}
+	complex(a, b, data);
 }
