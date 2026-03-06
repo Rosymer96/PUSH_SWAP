676fa39c4bedd96fb6ddba53eec6559cd6de72d7
albabmoran <albaben.moran@gmail.com>
2026-03-06 18:04:49 +0100

    deleted printf

--- alg_complex.c
+++ alg_complex.c
@@ -6,7 +6,7 @@
 /*   By: albben-a <albben-a@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 13:54:57 by rosvela           #+#    #+#             */
-/*   Updated: 2026/03/04 13:37:04 by albben-a         ###   ########.fr       */
+/*   Updated: 2026/03/06 18:04:26 by albben-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,4 @@ void	complex(t_stack **a, t_stack **b, t_data *data)
 	(void)a;
 	(void)b;
 	(void)data;
-	printf("complex: %s\n", (char *)data->stack_a);
 }
